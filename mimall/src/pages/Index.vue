00341c4a952bf8a92d3c91702d1fd25850a3161d
jone <eee@123.com>
2020-12-17 20:21:28 +0800

    轮播图session2

--- mimall/src/pages/Index.vue
+++ mimall/src/pages/Index.vue
@@ -2,74 +2,89 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
-         <swiper v-bind:options="swiperOption">
-            <swiper-slide v-for="(item,index) in slideList" :key="index">
-              <a v-bind:href="'/#/product'+item.id"><img :src="item.img" ></a>
-            </swiper-slide>
-             <div class="swiper-pagination"  slot="pagination"></div>
-             <div class="swiper-button-prev" slot="button-prev"></div>
-             <div class="swiper-button-next" slot="button-next"></div>
-         </swiper>
+        <swiper v-bind:options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a v-bind:href="'/#/product' + item.id"><img :src="item.img" /></a>
+           
+          </swiper-slide>
+         
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
       </div>
       <div class="ads-box"></div>
       <div class="banner"></div>
       <div class="product-box"></div>
     </div>
-   <service-bar></service-bar>
+    <service-bar></service-bar>
   </div>
 </template>
 <script>
-import ServiceBar from './../components/ServiceBar'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
+import ServiceBar from "./../components/ServiceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 export default {
-  name:'index',
-  components:{
-    swiper, 
+  name: "index",
+  components: {
+    swiper,
     swiperSlide,
     ServiceBar,
   },
   data() {
     return {
-      swiperOption:{
-
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6,
+        },
       },
-      slideList:[
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      slideList: [
         {
-        id:'42',
-        img:'/imgs/slider/slide-1.jpg'
-      },
-       {
-        id:'45',
-        img:'/imgs/slider/slide-2.jpg'
-      },
-       {
-        id:'46',
-        img:'/imgs/slider/slide-3.jpg'
-      },
-       {
-        id:'',
-        img:'/imgs/slider/slide-4.jpg'
-      },
-       {
-        id:'',
-        img:'/imgs/slider/slide-5.jpg'
-      },
-      ]
-    }
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-5.jpg",
+        },
+      ],
+    };
   },
-}
+};
 </script>
 <style lang="scss">
-  .index{
-    .swiper-box{
-      .swiper-container{
-        height: 451px;
-        img{
-          width:100%;
-          height:100%;
-        }
+.index {
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
       }
     }
   }
+}
 </style>
